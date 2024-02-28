67ddaa3f193c6f6b552ac032a79fc192a9c0f6b5
Dedemoglu Emre <emre@Emre.local>
2024-02-28 15:07:51 +0100

    2

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ededemog <ededemog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 00:14:25 by ededemog          #+#    #+#             */
-/*   Updated: 2024/02/27 00:15:39 by ededemog         ###   ########.fr       */
+/*   Updated: 2024/02/28 15:07:48 by ededemog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 int main(int argc, char **argv)
 {
+    t_list  *stack_a;
+    t_list  *stack_b;
 
+    stack_a = NULL;
+    stack_b = NULL;
+    if (argc < 2 || argc == 2 && !argv[1][0])
+        return (1);
+    else if (argc == 2)
+        argv = ft_split(argv[1], ' ');
 }
