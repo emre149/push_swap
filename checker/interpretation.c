cbf5ada432ec6ecbe77d25484e69e22d251df47b
emre149 <ededemog@student.42.fr>
2024-04-02 17:06:17 +0200

    checker almost finished

--- checker/interpretation.c
+++ checker/interpretation.c
@@ -6,7 +6,7 @@
 /*   By: ededemog <ededemog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 20:18:37 by ededemog          #+#    #+#             */
-/*   Updated: 2024/04/01 19:56:41 by ededemog         ###   ########.fr       */
+/*   Updated: 2024/04/02 15:20:24 by ededemog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,59 +15,87 @@
 #include "../push_swap.h"
 
 
-static void handle_swap(t_list **stack_a, t_list **stack_b, char *ligne)
+static bool handle_swap(t_list **stack_a, t_list **stack_b, char *ligne)
 {
     if (ft_strcmp(ligne, "pa") == 0)
-        pa(stack_a, stack_b, false);
+	{
+        pa(stack_a, stack_b, true);
+		return (true);	
+	}
     else if (ft_strcmp(ligne, "pb") == 0)
-        pb(stack_a, stack_b, false);
+	{
+        pb(stack_a, stack_b, true);
+		return (true);
+	}
+	return (false);
 }
 
-static void handle_rotate(t_list **stack_a, t_list **stack_b, char *ligne)
+static bool handle_rotate(t_list **stack_a, t_list **stack_b, char *ligne)
 {
     if (ft_strcmp(ligne, "ra") == 0)
-        ra(stack_a, false);
+	{
+        ra(stack_a, true);
+		return (true);
+	}
     else if (ft_strcmp(ligne, "rb") == 0)
-        rb(stack_b, false);
+	{
+        rb(stack_b, true);
+		return (true);
+	}
     else if (ft_strcmp(ligne, "rr") == 0)
-        rr(stack_a, stack_b, false);
+	{
+        rr(stack_a, stack_b, true);
+		return (true);
+	}
+	return (false);
 }
 
-static void handle_reverse_rotate(t_list **stack_a, t_list **stack_b, char *ligne)
+static bool handle_reverse_rotate(t_list **stack_a, t_list **stack_b, char *ligne)
 {
     if (ft_strcmp(ligne, "rra") == 0)
-        rra(stack_a, false);
+	{
+        rra(stack_a, true);
+		return (true);
+	}
     else if (ft_strcmp(ligne, "rrb") == 0)
-        rrb(stack_b, false);
+	{
+        rrb(stack_b, true);
+		return (true);
+	}
     else if (ft_strcmp(ligne, "rrr") == 0)
-        rrr(stack_a, stack_b, false);
+	{
+        rrr(stack_a, stack_b, true);
+		return (true);
+	}
+	return (false);
 }
 
-static void handle_push(t_list **stack_a, t_list **stack_b, char *ligne)
+static bool handle_push(t_list **stack_a, t_list **stack_b, char *ligne)
 {
     if (ft_strcmp(ligne, "sa") == 0)
-        sa(stack_a, false);
+	{
+        sa(stack_a, true);
+		return (true);
+	}
     else if (ft_strcmp(ligne, "sb") == 0)
-        sb(stack_b, false);
+	{
+        sb(stack_b, true);
+		return (true);
+	}
     else if (ft_strcmp(ligne, "ss") == 0)
-        ss(stack_a, stack_b, false);
+	{
+        ss(stack_a, stack_b, true);
+		return (true);
+	}
+	return (false);
 }
 
 int interpretor(t_list **stack_a, t_list **stack_b, char *ligne)
 {
-    bool state = false;
-
-    handle_swap(stack_a, stack_b, ligne);
-    if (state) return true;
-
-    handle_rotate(stack_a, stack_b, ligne);
-    if (state) return true;
-
-    handle_reverse_rotate(stack_a, stack_b, ligne);
-    if (state) return true;
-
-    handle_push(stack_a, stack_b, ligne);
-    if (state) return true;
+    if (handle_swap(stack_a, stack_b, ligne)) return true;
+    if (handle_rotate(stack_a, stack_b, ligne)) return true;
+    if (handle_reverse_rotate(stack_a, stack_b, ligne)) return true;
+    if (handle_push(stack_a, stack_b, ligne)) return true;
 
     return false;
 }
--- checker/main/checker.c
+++ checker/main/checker.c
@@ -6,56 +6,45 @@
 /*   By: ededemog <ededemog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 18:29:06 by ededemog          #+#    #+#             */
-/*   Updated: 2024/04/01 20:45:25 by ededemog         ###   ########.fr       */
+/*   Updated: 2024/04/02 15:46:18 by ededemog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include "gnl/get_next_line.h"
-#include "../push_swap.h"
-
-// void	checker(t_list **stack_a, t_list **stack_b, char *ligne)
-// {
-// 	int	state;
-// 	int	resultat;
-
-// 	resultat = 0;
-// 	while ((ligne = get_next_line(0) != NULL))
-// 	{
-// 		state = interpretor(stack_a, stack_b, ligne);
-// 		if (!state)
-// 			printf("Error, not an operation");
-// 		if (ft_strcmp(ligne, "Is that good ?") == 0)
-//             break;
-//         free(ligne);
-// 	}
-//     if (resultat == 0) {
-//         printf("KO\n");
-//     } else {
-//         printf("OK\n");
-//     }
-//     return 0;
-// }
+#include "../gnl/get_next_line.h"
+#include "../../push_swap.h"
 
 int	main(int argc, char **argv)
 {
-	int		resultat;
 	char	*ligne;
-	
-	(void)argc;
-	(void)argv;
+	t_list	*stack_a;
+	t_list	*stack_b;
 
-	resultat = 0;
-	while ((ligne = get_next_line(0) != NULL))
+	stack_a = NULL;
+	stack_b = NULL;
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
+		return (1);
+	else if (argc == 2)
+		argv = ft_split(argv[1], ' ');
+	split_to_stack(&stack_a, argv + 1);
+	if (stack_len(stack_a) < 2)
+		return (1);
+	while ((ligne = get_next_line(0)) != NULL)
 	{
-		if (ft_strcmp(ligne, "Is that good ?") == 0)
-            break;
+		if (ft_strcmp(ligne, "OK?") == 0)
+		{
+			if (is_sorted(stack_a) == false)
+				return (write(1, "KO\n", 3));
+			else
+				return (write(1, "OK\n", 3));
+		}
+		else
+		{
+			if (!interpretor(&stack_a, &stack_b, ligne))
+				write(1, "Error\n", 6);
+		}
         free(ligne);
 	}
-    if (resultat == 0) {
-        printf("KO\n");
-    } else {
-        printf("OK\n");
-    }
+	free_stacks(&stack_a);
     return 0;
 }
